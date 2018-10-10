6832e8d6a1503f3045a800df6bed9be36dda2898
duzhengbin <847479962@qq.com>
2018-10-10 14:23:30 +0800

    cmake 添加版本信息，增加注释

--- 0000/cmake_learn/src/tutorial.cxx
+++ 0000/cmake_learn/src/tutorial.cxx
@@ -7,7 +7,7 @@ int main (int argc, char *argv[]){
 		fprintf(stdout,"%s Version %d.%d\n",
             argv[0],
             Tutorial_VERSION_MAJOR,
-            Tutorial_VERSION_MINOR);
+            Tutorial_VERSION_MINOR);//打印版本信息
 		fprintf(stdout,"Usage: %s number\n",argv[0]);
 		return 1;
     }
