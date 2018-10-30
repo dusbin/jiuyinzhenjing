ca7319cf8290f0525de1e0b275e0ea29e26a8e3b
duzhengbin <847479962@qq.com>
2018-10-30 22:00:45 +0800

    增加ipfs学习笔记，使用os.Get_env方法获取系统环境变量代码

---
+++ 0002/gosrc/ipfs_learning/get_env.go
@@ -0,0 +1,10 @@
+package main
+import(
+	"fmt"
+	"os"
+)
+func main(){
+    //使用os.Getenv获取系统中的环境变量，通过key获取value
+	fmt.Println("user:[",os.Getenv("USER"),"]")
+	fmt.Println("pwd :[",os.Getenv("PWD"),"]")
+}
