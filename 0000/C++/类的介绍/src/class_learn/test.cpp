b2ed6668a46c86927d58b4d5d73d6b0f782e3b34
duzhengbin <847479962@qq.com>
2018-11-03 12:50:18 +0800

    实现熟悉的设置和获取

--- 0000/C++/类的介绍/src/class_learn/test.cpp
+++ 0000/C++/类的介绍/src/class_learn/test.cpp
@@ -5,22 +5,45 @@ public:
 	Cat(){
 		this->name = "undefine";
 	}
-	Cat(string name){
+	Cat(string name){//方法进行重载
 		this->name = name;
 	}
 	void Say(){//必须写返回值类型
-		cout << "i am " <<getname()<<", miao!"<<endl;
+		cout << "i am " <<getname()<<","<<SayMiao()<<endl;
+	}
+	int getNum(){
+		return this->shoutNum;
+	}
+	void SetNum(int value){//不实现set。表明数据只读
+		if(value <= 10)
+			this->shoutNum = value;
+		else
+			this->shoutNum = 10;
 	}
 private:
 	string getname(){
 		return this->name;
 	}
+	string SayMiao(){
+		string result = "";
+		for(int i= 0;i<this->shoutNum;i++){
+			result += "miao";
+			if(i<this->shoutNum-1){
+				result += " ";
+			}
+		}
+		return result;
+	}
 	string name;
+	int shoutNum = 3;
 };
 void test_01(){
 	Cat *cat = new Cat();
 	cat->Say();
 	Cat *cat2 = new Cat("Tian");
+	cat2->SetNum(5);
+	cat2->Say();
+	cat2->SetNum(11);
 	cat2->Say();
 }
 int main(){
