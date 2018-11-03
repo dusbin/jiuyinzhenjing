0223e5ef0438cb7e8e5e6fe429bd42f207b5a446
duzhengbin <847479962@qq.com>
2018-11-03 17:02:49 +0800

    通过虚函数实现多态

--- 0000/C++/类的介绍/src/class_learn/test.cpp
+++ 0000/C++/类的介绍/src/class_learn/test.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-class Cat{
+class Animal{
 public:
-	Cat(){
+	Animal(){
 		this->name = "undefine";
 	}
-	Cat(string name){//方法进行重载
+	Animal(string name){//方法进行重载
 		this->name = name;
 	}
 	void Say(){//必须写返回值类型
-		cout << "i am " <<getname()<<","<<SayMiao()<<endl;
+		cout << "i am " <<getname()<<","<<Saything()<<endl;
 	}
 	int getNum(){
 		return this->shoutNum;
@@ -20,11 +20,27 @@ public:
 		else
 			this->shoutNum = 10;
 	}
-private:
+protected://可以被继承
 	string getname(){
 		return this->name;
 	}
-	string SayMiao(){
+	string name;
+	int shoutNum = 3;
+private:
+	virtual string Saything(){
+		return "";
+	}
+};
+class Cat:public Animal{
+public:
+	Cat(){
+		this->name = "undefine";
+	}
+	Cat(string name){//方法进行重载
+		this->name = name;
+	}
+private:
+	string Saything(){
 		string result = "";
 		for(int i= 0;i<this->shoutNum;i++){
 			result += "miao";
@@ -34,8 +50,26 @@ private:
 		}
 		return result;
 	}
-	string name;
-	int shoutNum = 3;
+};
+class Dog:public Animal{
+public:
+	Dog(){
+		this->name = "undefine";
+	}
+	Dog(string name){//方法进行重载
+		this->name = name;
+	}
+private:
+	string Saything(){
+		string result = "";
+		for(int i= 0;i<this->shoutNum;i++){
+			result += "wang";
+			if(i<this->shoutNum-1){
+				result += " ";
+			}
+		}
+		return result;
+	}
 };
 void test_01(){
 	Cat *cat = new Cat();
@@ -45,6 +79,8 @@ void test_01(){
 	cat2->Say();
 	cat2->SetNum(11);
 	cat2->Say();
+	Dog *dog = new Dog("du");
+	dog->Say();
 }
 int main(){
 	test_01();
