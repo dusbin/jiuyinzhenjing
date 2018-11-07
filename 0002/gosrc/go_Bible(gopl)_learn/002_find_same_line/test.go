1d8f186633a61a71fb5de3a8acab3f4832f12b83
duzhengbin <847479962@qq.com>
2018-11-07 19:50:34 +0800

    查找重复的行并打印重复的此时，使用到map

---
+++ 0002/gosrc/go_Bible(gopl)_learn/002_find_same_line/test.go
@@ -0,0 +1,19 @@
+//cat 1|go run test.go
+package main
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+func main(){
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1{
+			fmt.Println(n,":",line)
+		}
+	}
+}
