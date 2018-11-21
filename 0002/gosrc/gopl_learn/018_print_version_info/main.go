ac7dda9a154383e5776d2ef467d6b5e666587982
duzhengbin <847479962@qq.com>
2018-11-21 16:33:45 +0800

    version包中存在问题

--- 0002/gosrc/gopl_learn/018_print_version_info/main.go
+++ 0002/gosrc/gopl_learn/018_print_version_info/main.go
@@ -1,19 +1,7 @@
 package main
 import(
-	//"./version"
-	"fmt"
-	"runtime"
-)
-var (
-	BuildTime 		= "0"
-	CommitID 		= "XXXXX"
+	"./version"
 )
 func main(){
-	//version.Version()
-	fmt.Println("Hello World")
-	fmt.Printf("Build time:\t%s\n",BuildTime)
-	fmt.Printf("Commit ID:\t%s\n",CommitID)
-	fmt.Printf("Go Version:\t%s\n",runtime.Version())
-	fmt.Printf("Go OS/Arch:\t%s/%s\n",runtime.GOOS,runtime.GOARCH)
-
+	version.Version()
 }
