27b5f27085a6a30899039b52b1dc72417d42db2f
duzhengbin <847479962@qq.com>
2018-11-21 16:47:14 +0800

    回退为在main中实现

--- 0002/gosrc/gopl_learn/018_print_version_info/main.go
+++ 0002/gosrc/gopl_learn/018_print_version_info/main.go
@@ -1,7 +1,19 @@
 package main
 import(
-	"./version"
+	//"./version"
+	"fmt"
+	"runtime"
+)
+var (
+	BuildTime 		= "0"
+	CommitID 		= "XXXXX"
 )
 func main(){
-	version.Version()
+	//version.Version()
+	fmt.Println("Hello World")
+	fmt.Printf("Build time:\t%s\n",BuildTime)
+	fmt.Printf("Commit ID:\t%s\n",CommitID)
+	fmt.Printf("Go Version:\t%s\n",runtime.Version())
+	fmt.Printf("Go OS/Arch:\t%s/%s\n",runtime.GOOS,runtime.GOARCH)
+
 }
