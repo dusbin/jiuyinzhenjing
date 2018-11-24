ede899d96b0d8090e0ec7c4ceb28dbd64b64b709
duzhengbin <847479962@qq.com>
2018-11-24 14:15:21 +0800

    新增加一个模块，实现系统语言获取，不需要修改模块外的代码

--- 0002/gosrc/gopl_learn/022_go_web/langinfo/langinfo.go
+++ 0002/gosrc/gopl_learn/022_go_web/langinfo/langinfo.go
@@ -1,4 +1,4 @@
-//+build userinfo
+//+build lang info
 
 package main
 import (
@@ -8,14 +8,14 @@ import (
 	"../html"
 )
 func Get_plugin_name()(pluginname string){
-	pluginname = "/userinfo"
+	pluginname = "/langinfo"
 	return
 }
 func Func_plugin(w http.ResponseWriter,r *http.Request){
 	fmt.Fprintf(w,"<html><body>")
-	html.Title(w,"user info")
+	html.Title(w,"lang info")
 	fmt.Fprintf(w,"<h1>info</h1>")
-	fmt.Fprintf(w,"<h2>user:%s<h2>",os.Getenv("USER"))
+	fmt.Fprintf(w,"<h2>lang:%s<h2>",os.Getenv("LANG"))
 	fmt.Fprintf(w,"</body></html>")
 	return
 }
