3e3aa14b299aa3ae066bfeb166383119a1f06b87
duzhengbin <847479962@qq.com>
2018-11-24 14:06:37 +0800

    实现index页面引导所有的网页

--- 0002/gosrc/gopl_learn/022_go_web/server/server.go
+++ 0002/gosrc/gopl_learn/022_go_web/server/server.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 )
 var count = 0
+var plugin_list = []string{}
 func Server(){
 	dir_list,error:=ioutil.ReadDir("./")
 	if error!= nil{
@@ -31,8 +32,12 @@ func Server(){
 			plugin_name:=get_plugin_name_func.(func()(string))()
 			fmt.Println("plugin name:",plugin_name)
 			plugin_func,err:=pdll.Lookup("Func_plugin")
+			if err != nil{
+				panic(err)
+			}
 			plg_func:=plugin_func.(func(http.ResponseWriter,*http.Request)())
 			http.HandleFunc(plugin_name,plg_func)
+			plugin_list = append(plugin_list,plugin_name)
 		}
 	}
 	http.HandleFunc("/",index)
@@ -42,6 +47,10 @@ func index(w http.ResponseWriter,r *http.Request){
 	fmt.Fprintf(w,"<html><body>")
 	html.Title(w,"main")
 	fmt.Fprintf(w,"<h1>info</h1>")
+	for _,v:=range plugin_list{
+		fmt.Fprintf(w,"<h2><a href=\"%s\">%s</a></h2>",v,v)
+	}
+	/*
 	flag:=1
 	if r.FormValue("name") != ""{
 		fmt.Fprintf(w,"<h2>name : %s</h2>",r.FormValue("name"))
@@ -54,6 +63,7 @@ func index(w http.ResponseWriter,r *http.Request){
 	if flag == 1{
 		fmt.Fprintf(w,"<h2>info is empty</h2>")
 	}
+	*/
 	fmt.Fprintf(w,"</body></html>")
 	return
 }
