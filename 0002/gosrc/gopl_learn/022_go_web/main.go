9c0586860d273b1cfd653682c293110bba4d5009
duzhengbin <847479962@qq.com>
2018-11-26 11:26:04 +0800

    实现服务的后台进行

--- 0002/gosrc/gopl_learn/022_go_web/main.go
+++ 0002/gosrc/gopl_learn/022_go_web/main.go
@@ -1,7 +1,19 @@
 package main
 import(
 	"./server"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 func main(){
+	if os.Getppid() != 1{
+		filePath,_:=filepath.Abs(os.Args[0])
+		cmd:=exec.Command(filePath,os.Args[1:]...)
+		//cmd.Stdin = os.Stdin
+		//cmd.Stdout = os.Stdout
+		//cmd.Stderr = os.Stderr
+		cmd.Start()
+		return
+	}
 	server.Server()
 }
