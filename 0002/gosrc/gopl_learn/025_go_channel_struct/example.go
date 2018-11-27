c421672ab3d244df0795773df11278fec00fb720
duzhengbin <847479962@qq.com>
2018-11-27 18:02:23 +0800

    增加一个例子

--- 0002/gosrc/gopl_learn/025_go_channel_struct/example.go
+++ 0002/gosrc/gopl_learn/025_go_channel_struct/example.go
@@ -1,5 +1,30 @@
 package main
+
+import (
+	"time"
+	"fmt"
+)
 func main(){
+	//test_01()
+	test_02()
+}
+//等待work的结束
+func test_02(){
+	done := make(chan struct{})
+	go func(){
+		work(5)
+		close(done)
+	}()
+	<-done
+}
+func work(v int){
+	for i := 0;i < v;i++{
+		time.Sleep(time.Second)
+		fmt.Println("wait")
+	}
+}
+//创建并且解除
+func test_01(){
 	var sig = make(chan struct{})
 	close(sig) //必须使用sig进行close，才能执行<-sig,否则会死锁
 	<-sig
