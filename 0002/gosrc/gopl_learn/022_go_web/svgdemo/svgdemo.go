1a52dc9b4f46d85f9b29cdef8e1011a5721c67d7
duzhengbin <847479962@qq.com>
2018-11-29 17:41:02 +0800

    实现3D sin(x)/x的图像

---
+++ 0002/gosrc/gopl_learn/022_go_web/svgdemo/svgdemo.go
@@ -0,0 +1,57 @@
+//+build svgdemo
+
+package main
+import (
+	"fmt"
+	"net/http"
+	"../html"
+	"math"
+)
+const
+ (
+    width, height = 600, 320// canvas size in pixels
+    cells         = 100// number of grid cells
+    xyrange       = 30.0// axis ranges (-xyrange..+xyrange)
+    xyscale       = width / 2 / xyrange // pixels per x or y unit
+    zscale        = height * 0.4 // pixels per z unit
+    angle         = math.Pi/6 // angle of x, y axes (=30°)
+)
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+func Get_plugin_name()(pluginname string){
+	pluginname = "/svgdemo"
+	return
+}
+func Func_plugin(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"<html><body>")
+	html.Title(w,"svgdemo")
+	fmt.Fprintf(w,"<h1>info</h1>")
+	fmt.Fprintf(w,"<svg xmlns='http://www.w3.org/2000/svg' style='stroke: red; fill: white; stroke-width: 0.7' width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+            ax, ay := corner(i+1, j)
+            bx, by := corner(i, j)
+            cx, cy := corner(i, j+1)
+            dx, dy := corner(i+1, j+1)
+            fmt.Fprintf(w,"<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",ax, ay, bx, by, cx, cy, dx, dy)
+        }
+    }
+    fmt.Fprintf(w,"</svg>")
+	fmt.Fprintf(w,"</body></html>")
+	return
+}
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+    x := xyrange * (float64 (i)/cells - 0.5)
+    y := xyrange * (float64 (j)/cells - 0.5)
+	// Compute surface height z.
+    z := f(x, y)
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+    sx := width/2 + (x-y)*cos30*xyscale
+    sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+func f(x, y float64) float64{
+    r := math.Hypot(x, y) 
+	// distance from (0,0)
+	return math.Sin(r) / r
+}
