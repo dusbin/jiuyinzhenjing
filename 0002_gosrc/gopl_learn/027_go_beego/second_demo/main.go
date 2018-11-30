53a136f438305b07f05da4487d447bdf50d06529
duzhengbin <847479962@qq.com>
2018-11-30 22:44:14 +0800

    注册一个路由“/”

--- 0002_gosrc/gopl_learn/027_go_beego/second_demo/main.go
+++ 0002_gosrc/gopl_learn/027_go_beego/second_demo/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	_ "second_demo/routers"
+	"second_demo/controllers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.Router("/",&controllers.MainController{})
 	beego.Run()
 }
 
