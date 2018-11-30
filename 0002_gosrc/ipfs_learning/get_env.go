1e064cd204cfc09f355ec16219595b60d5f92d2e
duzhengbin <847479962@qq.com>
2018-11-30 14:31:43 +0800

    获取当前路径

--- 0002_gosrc/ipfs_learning/get_env.go
+++ 0002_gosrc/ipfs_learning/get_env.go
@@ -7,4 +7,6 @@ func main(){
     //使用os.Getenv获取系统中的环境变量，通过key获取value
 	fmt.Println("user:[",os.Getenv("USER"),"]")
 	fmt.Println("pwd :[",os.Getenv("PWD"),"]")
+	currentPath,_ :=os.Getwd()
+	fmt.Println("GetWd()",currentPath)
 }
