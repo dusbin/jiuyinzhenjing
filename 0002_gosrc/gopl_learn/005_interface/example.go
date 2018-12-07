4f220f0b9534bb9f8c00ae005884156bf07edef3
duzhengbin <847479962@qq.com>
2018-12-07 23:18:08 +0800

    实现第二种方法，并不推荐

--- 0002_gosrc/gopl_learn/005_interface/example.go
+++ 0002_gosrc/gopl_learn/005_interface/example.go
@@ -18,10 +18,23 @@ func u_get(r Retriever) string {
 func u_add(r Retriever,url string) string {
 	return r.Add(url)
 }
-func main(){
+//第一种使用方法
+func test_01(){
 	var r Retriever
 	r = &retiver.Retriever{""} // 具体接口，并且进行初始化
 	u_set(r,"192.168.1.71") // 具体由retiver包实现
 	fmt.Println(u_get(r)) // 具体由retiver包实现
 	fmt.Println(u_add(r,":80")) // 具体由retiver包实现
+}
+// 第二种方法
+func test_02(){
+	// 以下三个retiver.Retriever{} 并不是一个  
+	// 输出为 "" ":80",也就是说u_set的结果u_get不到，u_get的结果u_add不了，所以不推荐这个方法
+	u_set(&retiver.Retriever{""},"192.168.1.72") //直接传入接口
+	fmt.Println(u_get(&retiver.Retriever{}))
+	fmt.Println(u_add(&retiver.Retriever{},":80"))
+}
+func main(){
+	test_01()
+	test_02()
 }
