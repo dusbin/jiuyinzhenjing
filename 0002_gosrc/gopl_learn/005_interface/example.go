4062c22bfa77860d9686806c37c0425dbc4cfa30
duzhengbin <847479962@qq.com>
2018-12-07 23:04:12 +0800

    第二中方式实现

--- 0002_gosrc/gopl_learn/005_interface/example.go
+++ 0002_gosrc/gopl_learn/005_interface/example.go
@@ -9,19 +9,19 @@ type Retriever interface{
 	Set(url string) //
 	Add(url string) string
 }
-func u_set(r Retriever){
-	r.Set("192.168.40.75")
+func u_set(r Retriever,url string){
+	r.Set(url)
 }
 func u_get(r Retriever) string {
 	return r.Get()
 }
-func u_add(r Retriever) string {
-	return r.Add(":80")
+func u_add(r Retriever,url string) string {
+	return r.Add(url)
 }
 func main(){
 	var r Retriever
 	r = &retiver.Retriever{""} // 具体接口，并且进行初始化
-	u_set(r) // 具体由retiver包实现
+	u_set(r,"192.168.1.71") // 具体由retiver包实现
 	fmt.Println(u_get(r)) // 具体由retiver包实现
-	fmt.Println(u_add(r)) // 具体由retiver包实现
+	fmt.Println(u_add(r,":80")) // 具体由retiver包实现
 }
