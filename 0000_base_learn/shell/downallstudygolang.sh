e4d1376e0657686f2ecf7eee798f9aa26a9d9c51
duzhengbin <847479962@qq.com>
2018-12-11 11:43:32 +0800

    调整获取到的文件名

--- 0000_base_learn/shell/downallstudygolang.sh
+++ 0000_base_learn/shell/downallstudygolang.sh
@@ -1,9 +1,25 @@
 # ! /bin/sh
+number(){
+        if [ $i -le 9 ];then
+                echo "0000"$i
+        else if [ $i -le 99 ];then
+                echo "000"$i
+        else if [ $i -le 999 ];then
+                echo "00"$i
+        else if [ $i -le 9999 ];then
+                echo "0"$i
+        else 
+                echo $i
+        fi
+	fi
+	fi
+	fi
+}
 i=1
 while [ $i -le 16827 ]
 do
 	curl "https://studygolang.com/articles/"$i -o "articles_"$i.html
-	title=$i_`cat articles_$i.html |grep "<title>"|awk -F'>' '{print$2}'|awk -F'<' '{printf$1}'`.html
+	title=`number`"_"`cat articles_$i.html |grep "<title>"|awk -F'>' '{print$2}'|awk -F'<' '{printf$1}'i|awk -F'/' '{printf$1"_"$2}'`.html
 	mv articles_$i.html "$title"
 	let i++
 done
