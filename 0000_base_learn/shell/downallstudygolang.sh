1a15cc4faad56668ede9fb91b9e0aabcf53f7b29
duzhengbin <847479962@qq.com>
2018-12-11 11:07:55 +0800

    更新脚本

--- 0000_base_learn/shell/downallstudygolang.sh
+++ 0000_base_learn/shell/downallstudygolang.sh
@@ -3,7 +3,7 @@ i=1
 while [ $i -le 16827 ]
 do
 	curl "https://studygolang.com/articles/"$i -o "articles_"$i.html
-	title=`cat articles_$i.html |grep "<title>"|awk -F'>' '{print$2}'|awk -F'<' '{printf$1}'`.html
+	title=$i_`cat articles_$i.html |grep "<title>"|awk -F'>' '{print$2}'|awk -F'<' '{printf$1}'`.html
 	mv articles_$i.html "$title"
 	let i++
 done
