1034a37348b6d14ec39fa11de46b2aa7f6861bf8
duzhengbin <847479962@qq.com>
2018-12-14 14:25:18 +0800

    堆排序查找最大的N个数

--- 0002_gosrc/gopl_learn/036_go_demo/heapsort/example.go
+++ 0002_gosrc/gopl_learn/036_go_demo/heapsort/example.go
@@ -1,13 +1,44 @@
 package main
+
 import (
 	"./heapsort"
 	"fmt"
 )
-func main(){
+
+/*
+	堆排序
+*/
+func test_01() {
 	//a := []int{3,2,5,4,1,6,8,7}
-	a := []int{3,2,5,4,1,6,8,7,11,123,12,14,21,4,234,1245,12,223,423}
+	a := []int{3, 2, 5, 4, 1, 6, 8, 7, 11, 123, 12, 14, 21, 4, 234, 1245, 12, 223, 423}
 	fmt.Println(a)
 	heapsort.Sort(a)
 	fmt.Println(a)
 	heapsort.Print(a)
+}
+
+/*
+	堆排序查找N大
+*/
+func test_02() {
+	a := []int{0, 0, 0, 0, 0, 0, 0, 0}
+	b := []int{12, 1, 3, 123, 111, 234, 1232, 121, 42, 1212, 2323, 13, 16, 12}
+	flag := 0
+	for _, v := range b {
+		if flag < len(a) {
+			a[flag] = b[flag]
+			flag += 1
+		} else if v > a[0] {
+			heapsort.Sort(a)
+			a[0] = v
+		}
+	}
+	heapsort.Sort(a)
+	heapsort.Sort(b)
+	fmt.Println(b)
+	fmt.Println(a)
+}
+func main() {
+	test_01()
+	test_02()
 }
--- 0002_gosrc/gopl_learn/036_go_demo/heapsort/heapsort/heapsort.go
+++ 0002_gosrc/gopl_learn/036_go_demo/heapsort/heapsort/heapsort.go
@@ -5,35 +5,35 @@ import (
 )
 
 func Sort(input []int) {
-	i_len_half := (len(input) -1)/ 2
+	i_len_half := (len(input) - 1) / 2
 	flag := 1
 	for flag == 1 {
 		flag = 0
-		for i :=0;i < i_len_half;i++{
+		for i := 0; i < i_len_half; i++ {
 			flag1 := 0
 			if 2*i+2 > len(input) {
-				flag1 = min(input[i],input[2*i+1],9999)
-			}else {
-				flag1 = min(input[i],input[2*i+1],input[2*i+2])
+				flag1 = min(input[i], input[2*i+1], 9999)
+			} else {
+				flag1 = min(input[i], input[2*i+1], input[2*i+2])
 			}
 			if flag1 == 2 {
-				input[i],input[2*i+1] = input[2*i+1],input[i]
+				input[i], input[2*i+1] = input[2*i+1], input[i]
 				flag = 1
 			}
 			if flag1 == 3 {
-				input[i],input[2*i+2] = input[2*i+2],input[i]
+				input[i], input[2*i+2] = input[2*i+2], input[i]
 				flag = 1
 			}
 		}
 	}
 }
 func Print(input []int) {
-	i_len_half := (len(input) -1)/ 2
-	for i :=0;i < i_len_half;i++{
-		fmt.Println(input[i],input[2*i+1],input[2*i+2])
+	i_len_half := (len(input) - 1) / 2
+	for i := 0; i < i_len_half; i++ {
+		fmt.Println(input[i], input[2*i+1], input[2*i+2])
 	}
 }
-func min(a,b,c int)(flag int){
+func min(a, b, c int) (flag int) {
 	m := a
 	flag = 1
 	if b < m {
@@ -44,5 +44,5 @@ func min(a,b,c int)(flag int){
 		m = c
 		flag = 3
 	}
-	return 
+	return
 }
