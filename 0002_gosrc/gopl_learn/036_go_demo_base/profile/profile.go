2a6413e16320ce79e938be4ba615348252bec672
duzhengbin <847479962@qq.com>
2018-12-20 18:29:15 +0800

    格式化代码格式

--- 0002_gosrc/gopl_learn/036_go_demo_base/profile/profile.go
+++ 0002_gosrc/gopl_learn/036_go_demo_base/profile/profile.go
@@ -1,69 +1,70 @@
 package main
 
 import (
-	"time"
 	"context"
-	"runtime/pprof"
-	"os"
-	"net/http"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"runtime/pprof"
+	"time"
 	//"net/http/pprof"
 )
-import()
-func main(){
+import ()
+
+func main() {
 	//test_01()
 	test_02()
 }
-func test_01(){
-	go func(){
+func test_01() {
+	go func() {
 		for {
 			fmt.Println("hello world")
 		}
 	}()
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func test_02() {
-    cpuf, err := os.Create("cpu_profile")
-    if err != nil {
-        log.Fatal(err)
-    }
-    pprof.StartCPUProfile(cpuf)
-    defer pprof.StopCPUProfile()
+	cpuf, err := os.Create("cpu_profile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(cpuf)
+	defer pprof.StopCPUProfile()
 
-    ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
-    test(ctx)
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+	test(ctx)
 
-    time.Sleep(time.Second * 3)
-    memf, err := os.Create("mem_profile")
-    if err != nil {
-        log.Fatal("could not create memory profile: ", err)
-    }
-    if err := pprof.WriteHeapProfile(memf); err != nil {
-        log.Fatal("could not write memory profile: ", err)
-    }
-    memf.Close()
+	time.Sleep(time.Second * 3)
+	memf, err := os.Create("mem_profile")
+	if err != nil {
+		log.Fatal("could not create memory profile: ", err)
+	}
+	if err := pprof.WriteHeapProfile(memf); err != nil {
+		log.Fatal("could not write memory profile: ", err)
+	}
+	memf.Close()
 }
 func test(c context.Context) {
-    i := 0
-    j := 0
-    go func() {
-        m := map[int]int{}
-        for {
-            i++
-            m[i] = i
-        }
-    }()
-    go func() {
-        m := map[int]int{}
-        for {
-            j++
-            m[i] = i
-        }
-    }()
-    select {
-    case <-c.Done():
-        fmt.Println("done, i", i, "j", j)
-        return
-    }
+	i := 0
+	j := 0
+	go func() {
+		m := map[int]int{}
+		for {
+			i++
+			m[i] = i
+		}
+	}()
+	go func() {
+		m := map[int]int{}
+		for {
+			j++
+			m[i] = i
+		}
+	}()
+	select {
+	case <-c.Done():
+		fmt.Println("done, i", i, "j", j)
+		return
+	}
 }
