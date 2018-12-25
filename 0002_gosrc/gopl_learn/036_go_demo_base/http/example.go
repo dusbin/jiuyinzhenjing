82fe44bac7a40de2712209866dd9ed955c898c62
duzhengbin <847479962@qq.com>
2018-12-25 17:55:14 +0800

    增加cookie的例子

--- 0002_gosrc/gopl_learn/036_go_demo_base/http/example.go
+++ 0002_gosrc/gopl_learn/036_go_demo_base/http/example.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
 	"net/http"
 )
 
+var (
+	addr = flag.String("addr", ":80", "server address")
+)
+
 func main() {
-	test_01()
+	//test_01()
+	test_cookie()
+}
+func test_cookie() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/get", getCookie)
+	mux.HandleFunc("/delete", deleteCookie)
+	mux.HandleFunc("/set", setCookie)
+	http.ListenAndServe(*addr, mux)
+}
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<a href="#" onclick="alert(document.cookie)">Click here!</a>`))
+}
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("this_is_a_test_cookie")
+	if err != nil {
+		w.Write([]byte("读取cookie失败: " + err.Error()))
+	} else {
+		data, _ := json.MarshalIndent(c, "", "\t")
+		w.Write([]byte("读取的cookie值: \n" + string(data)))
+	}
+	fmt.Println(c)
+}
+func deleteCookie(w http.ResponseWriter, r *http.Request) {
+	c := http.Cookie{
+		Name:   "this_is_a_test_cookie",
+		MaxAge: -1}
+	http.SetCookie(w, &c)
+	w.Write([]byte("cookie已被删除"))
+}
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c := http.Cookie{
+		Name:     "this_is_a_test_cookie",
+		Value:    "true",
+		HttpOnly: true,
+		//Secure:   true,
+		MaxAge: 300}
+	http.SetCookie(w, &c)
+	w.Write([]byte("cookie已创建\n"))
 }
 func test_01() {
 	http.Handle("/", http.FileServer(http.Dir("/")))
--- 0002_gosrc/gopl_learn/036_go_demo_base/http/notes.md
+++ 0002_gosrc/gopl_learn/036_go_demo_base/http/notes.md
@@ -2,3 +2,4 @@
 1. 文件服务的使用，参考example.go test_01() 
     `http.FileServer`
 [参考资料](https://www.cnblogs.com/hellosun/p/3887615.html)
+2. cookie的使用 [参考资料](https://studygolang.com/articles/17160)
