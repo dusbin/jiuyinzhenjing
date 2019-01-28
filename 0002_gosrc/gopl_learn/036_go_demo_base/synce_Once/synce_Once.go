1a6783c53c133a54d7bc90b41ef8721ad64a6cac
Bin-DuS <847379962@qq.com>
2019-01-28 21:57:06 +0800

    sync.Once 的使用，保证函数只执行一次

---
+++ 0002_gosrc/gopl_learn/036_go_demo_base/synce_Once/synce_Once.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+	sync.Once.Do(f func())是一个挺有趣的东西,能保证once只
+	执行一次，无论你是否更换once.Do(xx)这里的方法,
+	这个sync.Once块只会执行一次。
+	整个程序，只会执行onces()方法一次,
+	onced()方法是不会被执行的。
+	https://blog.csdn.net/x369201170/article/details/26019331
+*/
+var once sync.Once
+
+func main() {
+	for i, v := range make([]string, 10) {
+		once.Do(onces)
+		fmt.Println("count", v, "---", i)
+	}
+	for i := 0; i < 10; i++ {
+		go func() {
+			once.Do(onced)
+			fmt.Println("213")
+		}()
+	}
+	time.Sleep(4000)
+}
+func onces() {
+	fmt.Println("onces")
+}
+func onced() {
+	fmt.Println("onced")
+}
