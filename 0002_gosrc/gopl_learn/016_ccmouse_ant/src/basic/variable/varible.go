44600b2f5ad85604c886ba0049bbe730917e120b
Bin-DuS <847379962@qq.com>
2019-01-30 20:59:52 +0800

    变量的定义

--- 0002_gosrc/gopl_learn/016_ccmouse_ant/src/basic/variable/varible.go
+++ 0002_gosrc/gopl_learn/016_ccmouse_ant/src/basic/variable/varible.go
@@ -1,7 +1,43 @@
 package main
 
 import "fmt"
-
+// 包内部变量 只能用 var ；不能用 aa := 3
+/*
+var aa = 3
+var ss = "kkk"
+var bb = true
+*/
+var (
+	aa = 3
+ 	ss = "kkk"
+ 	bb = true
+)
+func variableZeroValue(){
+	var a int // 默认是0
+	var s string // 默认是空串
+	fmt.Printf("%q\n",s)
+	fmt.Println(a,s)
+}
+func variableInitialValue(){
+	var a, b int = 3,4 // 可以赋多值
+	var s string = "abc"
+	fmt.Println(a,s,b)
+}
+// 不规定类型
+func variableTypeDeduction(){
+	var a,b,c,s = 3,4,true,"def"
+	fmt.Println(a,b,c,s)
+}
+func variableShorter(){
+	a,b,c,s := 3,4,true,"def"
+	fmt.Println(a,b,c,s)
+	b = 5 // 不能在用 :=  定义时使用 := 这是赋值
+	fmt.Println(a,b,c,s)
+}
 func main() {
 	fmt.Println("hello world")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
 }
