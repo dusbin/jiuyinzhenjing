3f1969de8344d4bcfb7c493ef0c19e0eb478f5d2
Bin-DuS <847379962@qq.com>
2019-01-31 21:27:44 +0800

    2-5 循环学习

--- 0002_gosrc/gopl_learn/016_ccmouse_ant/src/basic/2-5-循环/loop.go
+++ 0002_gosrc/gopl_learn/016_ccmouse_ant/src/basic/2-5-循环/loop.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func convertToBin(n int) string {
+	result := ""
+	for ;n > 0;n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
 func main() {
-	$END$
+	fmt.Println(convertToBin(5))
+	fmt.Println(convertToBin(13))
 }
