5c419c2bdace5b11f1037965a18a9c7316c271d3
Bin-DuS <847379962@qq.com>
2019-02-18 22:15:27 +0800

    使用可变参数实现加法和平均算法

--- 0002_gosrc/gopl_learn/016_ccmouse_ant/src/basic/1-1-func/function.go
+++ 0002_gosrc/gopl_learn/016_ccmouse_ant/src/basic/1-1-func/function.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
+func sum(in ...int) int{
+	sum := 0
+	for _,v := range in {
+		sum += v
+	}
+	return sum
+}
+func avge(in ...int) int{
+	sum := 0
+	i := 0
+	for _,v := range in {
+		sum += v
+		i++
+	}
+	return sum/i
+}
+func opt(f func(...int)int,in ...int) int {
+	return f(in...)
+}
 func main() {
-	$END$
+	fmt.Println(opt(sum,1,2,3,4,5))
+	fmt.Println(opt(avge,1,2,3,4,5,6))
 }
